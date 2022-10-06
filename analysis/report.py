19e7ea3bf767fd511234491ec6a1f6e358fdb8c4
Alistair Jones <alistair.jones5@nhs.net>
2022-10-06 14:54:55 +0000

    Tidying

--- analysis/report.py
+++ analysis/report.py
@@ -2,9 +2,6 @@ import pandas as pd
 
 df = pd.read_csv("output/input.csv")
 
-# fig = df.SEX.plot.hist().get_figure()
-# fig.savefig("output/descriptive.png")
-
 sex_count_df = (
     df
     .groupby("patient_id", as_index=False)
@@ -14,7 +11,10 @@ sex_count_df = (
     .count()
 )
 sex_count_df.columns = ["NUMBER_OF_SEX_VALUES", "FREQUENCY"]
-print(sex_count_df)
 
-fig = sex_count_df.plot(kind="bar", x="NUMBER_OF_SEX_VALUES", y="FREQUENCY").get_figure()
+fig = (
+    sex_count_df
+    .plot(kind="bar", x="NUMBER_OF_SEX_VALUES", y="FREQUENCY")
+    .get_figure()
+)
 fig.savefig("output/descriptive.png")
