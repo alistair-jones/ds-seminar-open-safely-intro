e511d19c3e667a19b9510d48f4f6e495552b6089
Alistair Jones <alistair.jones5@nhs.net>
2022-10-06 14:50:04 +0000

    Make report more interesting

--- analysis/report.py
+++ analysis/report.py
@@ -1,6 +1,20 @@
 import pandas as pd
 
-data = pd.read_csv("output/input.csv")
+df = pd.read_csv("output/input.csv")
 
-fig = data.SEX.plot.hist().get_figure()
+# fig = df.SEX.plot.hist().get_figure()
+# fig.savefig("output/descriptive.png")
+
+sex_count_df = (
+    df
+    .groupby("patient_id", as_index=False)
+    ["SEX"]
+    .count()
+    .groupby("SEX", as_index=False)
+    .count()
+)
+sex_count_df.columns = ["NUMBER_OF_SEX_VALUES", "FREQUENCY"]
+print(sex_count_df)
+
+fig = sex_count_df.plot(kind="bar", x="NUMBER_OF_SEX_VALUES", y="FREQUENCY").get_figure()
 fig.savefig("output/descriptive.png")
